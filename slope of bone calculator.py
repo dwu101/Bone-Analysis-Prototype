371d83f628ff199a5b094fd9d68e5d96ab091fb5
dwu101 <105822234+dwu101@users.noreply.github.com>
2022-10-08 10:48:29 -0400

    Add files via upload

--- slope of bone calculator.py
+++ slope of bone calculator.py
@@ -8,51 +8,59 @@ import matplotlib.pyplot as plt
 import numpy as np
 from operator import itemgetter
 import pyautogui
-import win32api
+
 import time
 from PIL import Image
 
+global img
+img = cv2.imread("bone screenshot.jpg")
+
+
+
 def find_edge(x,y):
-    
-    
-    
+    threshold = 147
     while True: # finds the edge of the bone 
         if x <=0:
             # cv2.putText(img,"Bone not found",(50,50),2,0.5,(255,255,255),2)
             
             return "None"
             
-        elif (img[y,x][0] > 150 and img[y,x][1] > 150 and img[y,x][2] > 150).any():
-            if (img[y,x-1][0] > 150 and img[y,x-1][1] > 150 and img[y,x-1][2] > 150).any():
+        
+        if (img[y,x][0] > threshold and img[y,x][1] > threshold and img[y,x][2] > threshold).any():
+            if (img[y,x-1][0] > threshold and img[y,x-1][1] > threshold and img[y,x-1][2] > threshold).any():
             # cv2.circle(img, (x,y),10,(0,255,0), -1)
                 return[x,y]
        
         x-=1
+       
     
-    
-    
-def edge_finder(xp, image_name):
-    global img
-    img = cv2.imread(image_name)
-    
+def edge_finder(xp):
+
     size = img.shape
     x = size[1]
     y = size[0]
-    x = size[1]
-    y = size[0] 
+    print(x,y)
+
+ 
     edge_points = []
-    for i in range(1,150):
+    numpix = 1
+    pixaway = 1
+    direction = 1
+    while numpix <= 100:
         
         
-        append = find_edge(x-1, y//xp + ((-1)**i) * i)
+        append = find_edge(x-1, y-xp + (pixaway*direction))
         if append == "None":
-            append = find_edge(x-1,y//xp +((-1)**(i+1)*i))
-        
-        if append == "None":
-            break
+            direction = -1
+            pixaway = 1
+        else:           
+            edge_points.append(append)
+        pixaway +=1 
+        numpix +=1
+        if numpix == 10:
+            print("x")
 
-        edge_points.append(append)
-
+    print(edge_points)
         
     
     x=1
@@ -72,7 +80,7 @@ def edge_finder(xp, image_name):
     edge_points = (sorted(edge_points, key=itemgetter(1), reverse = True))
     while True:
         try:
-            if math.fabs(edge_points[x][1] - edge_points[x-1][1]) > 5 : 
+            if math.fabs(edge_points[x][1] - edge_points[x-1][1]) > 2 : 
                 del edge_points[x]
             else:
                 x+=1
@@ -82,6 +90,7 @@ def edge_finder(xp, image_name):
 
 
     return edge_points
+    
     
 
 def slope(points): #this doesnt work for 10 cuz the edge is straight. figure out a work around.
@@ -113,8 +122,10 @@ def point_drawer(points):
     global img
     for element in points:
         cv2.circle(img, (element[0],element[1]),1,(0,255,0), -1)
+        
+    img2 = cv2.resize(img, (700,800))
     while (1):
-        cv2.imshow("Slope Finder", img)
+        cv2.imshow("Slope Finder", img2)
         if cv2.waitKey(0) & 0xFF == 27:
             break
 
@@ -146,54 +157,91 @@ def angle_calculator(num1, num2):
     line3_len = length(point1, point2)
 
     angle_rad = math.acos((line1_len**2 + line2_len**2 - line3_len**2)/(2*line2_len*line1_len))
-    angle_deg = angle_rad * 180 / math.pi
+    angle_deg = round(angle_rad * 180 / math.pi, 2)
+    cv2.putText(img, "Angle: " + str(angle_deg) + "d", (10, 50),cv2.FONT_HERSHEY_COMPLEX, .5, (0,255,0), 1 )
+    img2 = cv2.resize(img, (700,800))
+    while (1):
+        cv2.imshow("Slope Finder", img2)
+        if cv2.waitKey(0) & 0xFF == 27:
+            break
+    
     return angle_deg
 
-def screenshot():
-    while True:
-        a = win32api.GetKeyState(0x11) # detects control click, starts taking screen shots 
-        time.sleep (0.01) 
-        if a < 0:
-            x = 1
-            while True:
-                myScreenshot = pyautogui.screenshot() # takes about 6 pictures per second
-                myScreenshot.save(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\{screenshot}_{x}.png'.format(x = x, screenshot = name ))
-                x +=1
 
-                b = win32api.GetKeyState(0x10) # detects right click
+# def screenshot():
+#     while True:
+#         a = win32api.GetKeyState(0x11) # detects control click, starts taking screen shots 
+#         time.sleep (0.01) 
+#         if a < 0:
+#             x = 1
+#             while True:
+#                 myScreenshot = pyautogui.screenshot() # takes about 6 pictures per second
+#                 myScreenshot.save(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\{screenshot}_{x}.png'.format(x = x, screenshot = name ))
+#                 x +=1
 
-                if b < 0: # crops image after shift click
-                    for i in range(1,x+1):
-                        im = Image.open(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\{screenshot}_{x}.png'.format(x = i, screenshot = name ))
+#                 b = win32api.GetKeyState(0x10) # detects right click
+
+#                 if b < 0: # crops image after shift click
+#                     for i in range(1,x+1):
+#                         im = Image.open(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\{screenshot}_{x}.png'.format(x = i, screenshot = name ))
                         
-                        left = 100
-                        top = 75
-                        right = 130
-                        bottom = 150 
+#                         left = 100
+#                         top = 75
+#                         right = 130
+#                         bottom = 150 
 
-                        im = im.crop((left, top, right, bottom))
+#                         im = im.crop((left, top, right, bottom))
 
-                        im.save(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\screenshot_{x}.png'.format(x = i))
+#                         im.save(r'C:\Users\danqw\OneDrive\Desktop\Screen.for.lab\screenshot_{x}.png'.format(x = i))
 
-                    return x
-
-
+#                     return x
 
 
-name = input("Description: ")
-x = screenshot(name)
-max_angle = 0 
-for i in range(x):
-    bottom = edge_finder(2, "screenshot_{x}".format(x=x)) # PUT THE FILE LOCATION HERE
+
+
+# name = input("Description: ")
+# x = screenshot(name)
+# max_angle = 0 
+# for i in range(x):
+#     bottom = edge_finder(2, "screenshot_{x}".format(x=x)) # PUT THE FILE LOCATION HERE
+#     bottom_slope = slope(bottom)
+#     top = edge_finder(10, "screenshot_{x}".format(x=x))
+#     top_slope = slope(top)
+
+#     angle = angle_calculator(bottom_slope, top_slope) 
+#     if angle > max_angle:
+#         max_angle = angle 
+
+# print(angle)
+
+
+
+
+
+
+
+
+
+
+
+
+def run_code():
+
+    bottom = edge_finder(150)
+    # print(bottom)
+    point_drawer(bottom)
     bottom_slope = slope(bottom)
-    top = edge_finder(10, "screenshot_{x}".format(x=x))
+    print(bottom_slope)
+
+
+    top = edge_finder(350)
+    # print(top)
+    point_drawer(top)
     top_slope = slope(top)
+    # print(top_slope)
+    
 
-    angle = angle_calculator(bottom_slope, top_slope) 
-    if angle > max_angle:
-        max_angle = angle 
-
-print(angle)
+    angle_calculator(bottom_slope, top_slope)
 
 
 
@@ -204,22 +252,5 @@ print(angle)
 
 
 
-
-
-
-
-# bottom = edge_shower(2)
-# print(bottom)
-# point_drawer(bottom)
-# bottom_slope = slope(bottom)
-# print(bottom_slope)
-
-
-# top = edge_shower(10)
-# print(top)
-# point_drawer(top)
-# top_slope = slope(top)
-# print(top_slope)
-
-# print(angle_calculator(bottom_slope, top_slope))
+run_code()
 
